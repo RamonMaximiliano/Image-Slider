bcf5d999fe234474507c7b0aca78525b2dce2248
Ramon Maximiliano Wilbert <ramon_max1986@hotmail.com>
2021-11-20 22:58:36 -0300

    Finished Slider

--- animation.js
+++ animation.js
@@ -1,3 +1,5 @@
+//pensei que havia bug neste código porém não, você tem que esperar a imagem aparecer por completo antes de clicar para vir a proxima
+
 function changeImageRight() {
     const mainImage = document.getElementById("mainImage")
 
@@ -39,3 +41,48 @@ function changeImageLeft() {
         mainImageLeft.classList.add("image1")
     }
 }
+
+
+/*Vai o nome da função e o intervalo em milisegundos de quanto em quanto tempo ela deve rodar*/
+setInterval(changeImageRight, 15000)
+
+/*Acima meu código*/
+
+
+
+/*Abaixo código do Boni*/
+
+let totalSlides = document.querySelectorAll("div.sliderItem").length
+let currentSlide = 0;
+let sliderWidth = document.querySelector("div.sliderItem").clientWidth
+
+
+
+
+function changeImageLeft1() {
+    console.log(totalSlides)
+    currentSlide++;
+    if(currentSlide > (totalSlides -1)) {
+        currentSlide = 0;
+    } 
+    updateMargin()
+}
+
+function changeImageRight2() {
+    console.log(totalSlides)
+    currentSlide--;
+    if(currentSlide < 0) {
+        currentSlide = totalSlides -1;
+    } 
+    updateMargin()
+}
+
+function updateMargin(){
+    /*    let newMargin = (currentSlide * document.body.clientWidth); */
+    let newMargin = (currentSlide * document.body.clientWidth);
+    document.querySelector("div.sliderBoni").style.marginLeft = `-${newMargin}px`;
+}
+
+
+/*Vai o nome da função e o intervalo em milisegundos de quanto em quanto tempo ela deve rodar*/
+setInterval(changeImageRight2, 15000)
